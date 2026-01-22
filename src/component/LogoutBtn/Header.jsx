07026307375f74ca035e6a617d57d789353aa54f
kanhaiya kumar <kk7077574@gmail.com>
2026-01-22 14:29:12 +0530

    Added react components for input and header

--- src/component/LogoutBtn/Header.jsx
+++ src/component/LogoutBtn/Header.jsx
@@ -8,20 +8,20 @@ import { useNavigate } from 'react-router-dom'
 export default function Header() {
   const authStatus = useSelector((state) => state.auth.state)
   const navigate = useNavigate()
-  const naItems = [
+  const navItems = [
     {
-      name:'Home',
+      name: 'Home',
       slug: "/",
       active: true
     },
     {
-      name:"Login",
-      slug:"/login",
+      name: "Login",
+      slug: "/login",
       active: !authStatus,
     },
     {
-      name:"Signup",
-      slug:"/signup",
+      name: "Signup",
+      slug: "/signup",
       active: !authStatus,
     },
     {
@@ -36,6 +36,38 @@ export default function Header() {
     },
   ]
   return (
-    <div>Header</div>
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo width='70px' />
+            </Link>
+          </div>
+          <ul className='flex ml-auto'>
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                  onClick={()=> navigate(item.slug)}
+                  className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>
+                    {item.name}
+                  </button>
+                </li>
+              ):(
+                null
+              )
+            )
+            }
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+
+          </ul>
+        </nav>
+      </Container>
+    </header>
   )
 }
